51cefb5daf6795c98992b0ded84a030dddc53b58
Nivas Muthu M G <nivasmuthumg@icloud.com>
2021-07-05 05:06:18 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Nivas Muthu M G on 05/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
